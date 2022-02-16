fe2038c2224acef557c58326b1fb1a009cdc5ea8
RFilho01 <robertocorreiafilho0100@outlook.com>
2022-02-16 12:47:03 -0300

    fixing home suite testes

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,6 +3,7 @@
 
 describe('home page', () => {
     it('app deve estar online', () => {
+        cy.get('/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
     })
 })
